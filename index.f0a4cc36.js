2f427ef50c1fb4c32d117cd118e99930f62baeb2
Misha Remeslo <misha.remeslol@gmail.com>
2024-07-24 22:00:43 +0300

    make build

---
+++ index.f0a4cc36.js
@@ -0,0 +1,2 @@
+document.querySelector("div");var e=document.querySelector("img");e.style.top="50%",e.style.left="50%",e.style.transform="translate(-50%, -50%)";
+//# sourceMappingURL=index.f0a4cc36.js.map
